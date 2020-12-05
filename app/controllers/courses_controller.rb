1ddc33f84739289fe7f650dfba848d6b6c0dfdbd
Cobbeec <cobbeec@gmail.com>
2020-12-05 14:29:27 -0600

    added more CRUD functionality

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,15 +1,16 @@
 class CoursesController < ApplicationController
- get '/courses' do
+ 
+get '/courses' do
     @courses = Course.all
         erb :'/courses/index'
     end
 
- get '/courses/new' do
+get '/courses/new' do
     @courses = Course.all
          erb :'courses/new'
        end
  
- get '/courses/:id' do
+get '/courses/:id' do
      @course = Course.find_by_id(params[:id])
        erb :'/courses/show'
      end
@@ -20,18 +21,20 @@ get '/courses/:id/edit' do
     end
 
 post '/courses' do
-    @course = Course.create(params["course"])
-        if !params["student"]["name"].empty?
-    @course.student = Course.create(name: params["student"]["name"])
-        end
-        @course.save
-        redirect to "courses/#{@course.id}"
-        end
+    @course = Course.create(params)
+    redirect to "/courses/#{@course.id}"
+    end
 
 patch '/courses/:id' do 
     course = Course.find(params[:id])
     course.update(params[:course])
     redirect to "/courses/#{courses.id}"
+    end 
+
+delete '/courses/:id' do 
+    course = Course.find(params[:id])
+    Course.destory(params[:id])
+    redirect to '/courses' 
     end 
 
 end 
