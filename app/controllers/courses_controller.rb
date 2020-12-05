a070dfd962b43f8fdd88efe589235406993732ee
Cobbeec <cobbeec@gmail.com>
2020-12-05 10:22:18 -0600

    added routes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,12 @@
 class CoursesController < ApplicationController
+ get '/courses' do
+        @courses = Course.all
+        erb :'/courses/index'
+    end
+
+ get '/courses/new' do
+      @courdes = Course.all
+         erb :'/courses/new'
+       end
+
 end 
